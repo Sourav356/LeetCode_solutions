e6ad99192fa484732477d09ff3ee6c69cef316d0
Sourav Mallick <137917513+Sourav356@users.noreply.github.com>
2023-12-19 21:12:11 +0530

    Time: 500 ms (53.12%), Space: 17 MB (59.70%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,19 @@
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        m , n = len(img) , len(img[0])
+        result = [[0]*n for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                total = 0
+                count = 0
+                for x in range(max(0, i-1) , min(m, i+2)):
+                    for y in range(max(0, j-1) , min(n, j+2)):
+                        total +=img[x][y]
+                        count+=1
+                        
+                result[i][j] = total // count
+                
+        return result
+                
+        
