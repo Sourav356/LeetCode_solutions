28099f3f2f831b4df92c99065c2c3b915ab138e7
Sourav Mallick <137917513+Sourav356@users.noreply.github.com>
2024-01-03 00:08:27 +0530

    Time: 61 ms (5.92%), Space: 17.2 MB (17.27%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,4 +1,9 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         return n >0 and (n & (n - 1)) == 0
+    
+    
+    # or You can import math 
+    # Then run 
+    # return n > 0 and math.log2(n).is_integer()
         
