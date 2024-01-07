23661dce55ba3f90c361d602b7f5227f0c3560d3
Sourav Mallick <137917513+Sourav356@users.noreply.github.com>
2024-01-07 23:54:26 +0530

    Time: 31 ms (94.47%), Space: 17.4 MB (8.20%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,8 +1,27 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        return n >0 and (n & (n - 1)) == 0
+        number = n
+        if n == 0:
+            return False
+        new_divi = []
+        while number%2 == 0:
+            divi = number//2
+            new_divi .append(divi)
+            number = divi
+
+        count = len(new_divi)
+        print(count)
+        if 2**count == n:
+            return True
+        else:
+            return False
+        
+        
+        
+        
+        
     
-    
+    # return n >0 and (n & (n - 1)) == 0
     # or You can import math 
     # Then run 
     # return n > 0 and math.log2(n).is_integer()
