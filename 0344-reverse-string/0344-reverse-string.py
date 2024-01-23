dca6e8e667a3d5ca2e6970a3eb9a8e58a400297f
Sourav Mallick <137917513+Sourav356@users.noreply.github.com>
2024-01-23 23:14:03 +0530

    Time: 176 ms (60.95%), Space: 20.8 MB (67.94%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,14 +3,25 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        # s.reverse()
+        #here the Tm_COm is O(n) and Sp_com is O(1)
+        s.reverse()
         
         
-        #or
+        #or here the Tm_COm is O(n) and Sp_com is O(1)
         
-        left,right= 0, len(s) -1
-        while left < right:
-            s[left],s[right]=s[right],s[left]
-            left+=1
-            right-=1
+        # left,right= 0, len(s) -1
+        # while left < right:
+        #     s[left],s[right]=s[right],s[left]
+        #     left+=1
+        #     right-=1
         
+        
+        #OR here the Tm_COm is O(n) and Sp_com is O(N) thats why this is not give the right output where but in other places it gives the right output
+        # list_string =""
+        # string_list=[]
+        # for i in s:
+        #     list_string+=i
+        # reverse = list_string[::-1]
+        # for char in reverse:
+        #     string_list.append(char)
+        # return string_list
