304c12a636fee0625121cc4892900af3e3569937
Sourav Mallick <137917513+Sourav356@users.noreply.github.com>
2024-05-31 21:25:54 +0530

    Time: 62 ms (35.93%), Space: 18.4 MB (76.83%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,12 +1,30 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        dic = {}
-        for num in nums:
-            dic[num] = dic.get(num,0)+1
-        app = []
-        for num, count in dic.items():
-            if count == 1:
-                app.append(num)
+        #This the normal approach where it doesnot follow this condition You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+#         dic = {}
+#         for num in nums:
+#             dic[num] = dic.get(num,0)+1
+#         app = []
+#         for num, count in dic.items():
+#             if count == 1:
+#                 app.append(num)
 
-        return app
+#         return app
+    
+    # The actual approach is 
+        xor_all = 0
+        for num in nums:
+            xor_all ^= num
+            
+        diff_bit = xor_all & (-xor_all)
+        
+        unique1 = 0
+        unique2 = 0
+        
+        for num in nums:
+            if num & diff_bit:
+                unique1 ^=num
+            else:
+                unique2 ^=num
+        return [unique1, unique2]
         
